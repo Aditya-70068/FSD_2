df0aa1cfcc872f383cc7a78dc235817a5fa8ae3a
Aditya-70068 <Adityaawasthi0503@gmail.com>
2026-01-14 10:00:06 +0530

    first commit

---
+++ exp-1/src/App.jsx
@@ -0,0 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import "./App.css";   // if this line exists, it's fine
+
+function App() {
+  return (
+    <div className="app">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
